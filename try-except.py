45f85cd8b5a2a5ae02ef96faa5c3ee2827055ac4
lilseniorj <jesusvarguer18@gmail.com>
2026-02-24 17:04:37 -0500

    Control de errores en Python con try y except

---
+++ try-except.py
@@ -0,0 +1,12 @@
+a = 0
+b = 0
+
+try:
+    a = int(input("Digita un numero: "))
+    b = int(input("Digita otro numero: "))
+    resultado = a / b
+    print(f"El resultado: {resultado}")
+except ValueError:
+    print("El valor que digitó no es un número válido.")
+except ZeroDivisionError:
+    print("Error: No se puede dividir por cero.")
