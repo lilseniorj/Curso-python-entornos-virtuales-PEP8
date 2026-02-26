820db6f1240ec8ae175953a633f0e4ed8eacb5a8
lilseniorj <jesusvarguer18@gmail.com>
2026-02-26 16:11:28 -0500

    Tipado de funciones y estructuras de datos en Python

--- type-hints.py
+++ type-hints.py
@@ -3,13 +3,13 @@
 variable = 42 # int
 print(f"variable: {variable}, del tipo: {type(variable)}")
 
-variable = "Texto de prueba" # str
+variable = "" # str
 print(f"variable: {variable}, del tipo: {type(variable)}")
 
 otra_variable: int = 44
 print(f"variable: {otra_variable}, del tipo: {type(otra_variable)}")
 
-otra_variable = ""
+otra_variable: str = ""
 
 user_id: int | None = None
 
@@ -17,3 +17,25 @@ user_id: int | None = None
 # variable = valor
 # variable: tipo = valor
 
+def suma_clara (a: int,b: int) -> int:
+  return a + b
+
+
+articles: list[dict] = [
+  {"title": "expample"},
+  {"title": "example2"},
+]
+
+articles_dos: list[list[str]] = [
+  ["title", "expample"],
+  ["title", "example2"],
+]
+
+from typing import Any
+
+articles_tre: list[list[Any]] = [
+  ["title", "expample", 123],
+  ["title", "example2"],
+]
+
+#int, str, list, dict, tuple. Any
